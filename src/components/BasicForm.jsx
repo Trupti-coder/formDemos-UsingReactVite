75761782b226f4e5b4d63da83c074c5b78864659
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-02 14:32:26 -0400

    Update

--- src/components/BasicForm.jsx
+++ src/components/BasicForm.jsx
@@ -4,13 +4,19 @@ function BasicForm(){
     let[name,setName]=useState("");
     let[email,setEmail]=useState("");
     let[password,setPassword]=useState("");
-    
+
+
+    function funSubmit(event){
+
+    }
+
+
     return(
         <>
         <h2>Basic Form In React</h2>
-        <form>
+        <form onSubmit={funSubmit}>
             <div>
-                Name:<input type="text" placeholder="Enter Name" />
+                Name:<input type="text" placeholder="Enter Name" onChange={(event)=>setName(event.target.value)} />
             </div>
             <div>
                 Email:<input type="email" placeholder="Enter email" required />
