02bff9d60c358ca5d8985235daf6cd34bdab863c
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-02 14:39:49 -0400

    Update

--- src/components/BasicForm.jsx
+++ src/components/BasicForm.jsx
@@ -25,13 +25,13 @@ function BasicForm(){
         <h2>Basic Form In React</h2>
         <form onSubmit={funSubmit}>
             <div>
-                Name:<input type="text" placeholder="Enter Name" onChange={(event)=>setName(event.target.value)} />
+                Name:<input type="text" name="name" onChange={(event)=>setName(event.target.value)} />
             </div>
             <div>
-                Email:<input type="email" placeholder="Enter email" required onChange={(event)=>setEmail(event.target.value)} />
+                Email:<input type="email" name="email" required onChange={(event)=>setEmail(event.target.value)} />
             </div>
             <div>
-                Password:<input type="password" placeholder="Enter password" required  onChange={(event)=>setPassword(event.target.value)}/>           
+                Password:<input type="password"  name="password" required  onChange={(event)=>setPassword(event.target.value)}/>           
              </div>
              <button type="submit">Submit</button>
             
