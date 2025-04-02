511816d38af703ee1549eea72836e9cf3b7530f7
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-02 14:28:07 -0400

    Update BasicForm.jsx

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,6 +1,19 @@
 function BasicForm(){
     return(
         <>
+        <h2>Basic Form In React</h2>
+        <form>
+            <div>
+                Name:<input type="text" placeholder="Enter Name" />
+            </div>
+            <div>
+                Email:<input type="email" placeholder="Enter email" required />
+            </div>
+            <div>
+                Password:<input type="password" placeholder="Enter password" required />           
+             </div>
+            
+        </form>
         
         </>
     );
