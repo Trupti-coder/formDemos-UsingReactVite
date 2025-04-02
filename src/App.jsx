c9f27ebc9a178b4ac666c613fa904f22ed4de7a5
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-02 14:15:39 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import First from './components/First.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+    <First />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/First.jsx
@@ -0,0 +1,8 @@
+function First(){
+    return(
+        <>
+        <h3>Hello This is my first reactVite demo</h3>
+        </>
+    );
+}
+export default First;
