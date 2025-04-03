ce3c07bff0ea1a5dab613b99b6a33abcc9b057ec
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-02 22:00:19 -0400

    Update

--- src/components/IntermediateForm.jsx
+++ src/components/IntermediateForm.jsx
@@ -11,11 +11,21 @@ function IntermediateForm(){
                     City:<input type="text" name="city" />
                 </div>
                 <div>
+                    Gender:<select name="gender">
+                        <option value=""></option>
+                        <option value="male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="other">other</option>
+                    </select>
+                </div>
+                <div>
                     Email:<input type="email" name="email" required />
                 </div>
                 <div>
                     Password:<input type="password" name="password" required />
                 </div>
+
+                <button type="submit">Register</button>
             </form>
         </div>
         </>
