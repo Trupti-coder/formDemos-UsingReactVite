758984c1601802468e1e31b04ea489c66919f68f
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-03 12:58:50 -0400

    Update

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 function SignIn(){
+    let[email,setemail]=useState("");
+    let[password,setPassword]=useState("");
+    let[passwordError,setPasswordError]=useState("");
+
+    
     return(
         <>
         </>
