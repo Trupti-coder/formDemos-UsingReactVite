0765ddbdfa9bce0785d72c7a1e2669531fee1a00
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-02 21:52:46 -0400

    Update

--- src/components/IntermediateForm.jsx
+++ src/components/IntermediateForm.jsx
@@ -2,7 +2,21 @@ function IntermediateForm(){
     return(
         <>
         <div>
-            
+            <h3>Intermediate Form</h3>
+            <form>
+                <div>
+                    Name:<input type="text" name="name" required />
+                </div>
+                <div>
+                    City:<input type="text" name="city" />
+                </div>
+                <div>
+                    Email:<input type="email" name="email" required />
+                </div>
+                <div>
+                    Password:<input type="password" name="password" required />
+                </div>
+            </form>
         </div>
         </>
     );
