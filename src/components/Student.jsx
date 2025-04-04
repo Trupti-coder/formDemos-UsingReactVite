ceb4aff14860c7d77d613f923590cf0ea85dbe81
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-04 13:21:42 -0400

    Update

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 function Student(){
+    let[firstName,setFirstName]=useState("");
+    let[lastName,setLastName]=useState("");
+    let[email,setEmail]=useState("");
+    let[password,setPassword]=useState("");
     return(
         <>
         </>
