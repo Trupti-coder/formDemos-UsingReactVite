09e53112270bc024b2a2e4f5aa442fdc872ab114
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-04 13:27:31 -0400

    Update

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -5,8 +5,23 @@ function Student(){
     let[lastName,setLastName]=useState("");
     let[email,setEmail]=useState("");
     let[password,setPassword]=useState("");
+
     return(
         <>
+        <form>
+            <div>
+                FirstName:<input type="text" name="firstname" required />
+            </div>
+            <div>
+               LastName:<input type="text" name="lastname" required />
+            </div>
+            <div>
+                Email:<input type="email" name="email" required />            
+            </div>
+            <div>
+                Password:<input type="text" name="password" required />
+            </div>
+        </form>
         </>
     );
 }
