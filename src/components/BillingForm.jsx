675a2e9318c7bbc0c1807c576e507978397c54c7
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-06 10:52:42 -0400

    Update BillingForm.jsx

--- src/components/BillingForm.jsx
+++ src/components/BillingForm.jsx
@@ -1,9 +1,18 @@
 function BillingForm(){
     return(
         <>
+        <div>
+            <form>
+                <div>
+                    FirstName:<input type="text" name="firstname" required />
+                </div>
+                <div>
+                    LastName:<input type="text" name="lastname" required />                
+                </div>
+            </form>
 
+        </div>
 
-        
         </>
     );
 }
