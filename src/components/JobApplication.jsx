15ec771048e1e7c62df3ef5a46b3ef5697d72e95
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-07 15:07:26 -0400

    Update

--- src/components/JobApplication.jsx
+++ src/components/JobApplication.jsx
@@ -1,6 +1,13 @@
 function JobApplication(){
     return(
         <>
+        <div>
+            <h1>Job Application Form </h1>
+            <h4>Thank you for your interest in working with us.</h4>
+            <form>
+
+            </form>
+        </div>
         </>
     );
 }
