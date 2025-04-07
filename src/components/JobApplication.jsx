a3c8c15adc8fda96bbf215ee5bd95c78f1ad4237
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-07 15:10:28 -0400

    Update

--- src/components/JobApplication.jsx
+++ src/components/JobApplication.jsx
@@ -3,7 +3,8 @@ function JobApplication(){
         <>
         <div>
             <h1>Job Application Form </h1>
-            <h4>Thank you for your interest in working with us.</h4>
+            <h4>Thank you for your interest in working with us.
+                Please check below for available job opportunities that meet your criteria and send your application by filling out the job Application Form</h4>
             <form>
 
             </form>
