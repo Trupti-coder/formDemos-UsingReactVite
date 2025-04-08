ecf8b9ae30e71d22e5e5ba3255b659ca3c40d205
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-08 14:38:17 -0400

    Update

--- src/components/AdmissionForm.jsx
+++ src/components/AdmissionForm.jsx
@@ -1,7 +1,15 @@
 function AdmissionForm(){
     return(
         <>
-        
+        <div>
+            <h1>COMPLEX ACADEMY</h1>
+            <p>Another way to Education</p>
+            <h3>EDUCATIONAL & IT TRAINING ACADEMY</h3>
+            <h4><b>ADMISSION FORM</b></h4>
+
+
+        </div>
+
         </>
     );
 }
