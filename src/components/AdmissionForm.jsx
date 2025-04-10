95a4f83f61fc0969c74d632f5434eaba51c555f6
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-10 12:20:40 -0400

    Updated

--- src/components/AdmissionForm.jsx
+++ src/components/AdmissionForm.jsx
@@ -126,7 +126,7 @@ function AdmissionForm(){
             <div>
                 Gender:<input type="radio" name="gender" value="Male" checked={formdata.gender==="Male"} onChange={handleChange} />Male
                 <input type="radio" name="gender" value="Female" checked={formdata.gender==="Female"} onChange={handleChange} />Female
-                <input type="radio" name="gender" value="Other" checked={formdata.gender==="other"} onChange={handleChange} />Other
+                <input type="radio" name="gender" value="Other" checked={formdata.gender==="Other"} onChange={handleChange} />Other
 
             </div>
             <div>
