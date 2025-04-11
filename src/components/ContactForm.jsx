951c18e16ca79ad8e630a630b9ee4f4b926b95c0
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-11 18:51:54 -0400

    Update

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom";
+
 function ContactForm(){
     return(
         <>
+       <div>
+        <h1>Hey,it takes less that 60s</h1>
+        <p>Already have an account ? <Link>Log in</Link></p>
+        <form>
+            <div>
+               Your  Name:<input type="text" name="name" />
+            </div>
+            <div>
+
+            </div>
+        </form>
+       </div>
         </>
     );
 }
