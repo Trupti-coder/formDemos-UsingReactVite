325f7a6c24dd5b7bfa5786bcb74f9fb88d1f0fa3
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-12 22:13:35 -0400

    Update

--- src/components/ComunityForm.jsx
+++ src/components/ComunityForm.jsx
@@ -1,6 +1,18 @@
 function ComunityForm(){
     return(
         <>
+        <div>
+            <h1>Sign Up</h1>
+            <p>Join our community</p>
+            <form>
+                <div>
+                 First Name:<input type="text" />
+                </div>
+                <div>
+                    Last Name:<input type="text" />
+                </div>
+            </form>
+        </div>
         </>
     );
 }
