e0fefb384c1f79423ce18bad663c2f23a60049d4
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-14 13:33:12 -0400

    Update

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,7 +2,17 @@ function LoginForm(){
     return(
         <>
         <div>
-           
+           <form>
+            <div>
+                Name:<input type="text" />
+            </div>
+            <div>
+                City:<input type="text" />
+            </div>
+            <div>
+                Age:<input type="number" />
+            </div>
+           </form>
         </div>
         </>
     );
