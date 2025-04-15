1211c69bc8c60a255731a0842c758170f6764e35
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-15 09:11:22 -0400

    Update

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -11,6 +11,23 @@ const UserForm = () => {
             <div>
                 Name:<input type='text' />
             </div>
+            <div>
+                City:<input type='text' />
+            </div>
+            <div>
+                Age:<input type='text' />
+            </div>
+            <div>
+                <input type='radio' />Male 
+                <input type='radio' />Female 
+                <input type='radio' />Other
+            </div>
+            <div>
+                Emai:<input type='email' />
+            </div>
+            <div>
+                Password:<input type='password' />
+            </div>
         </form>
     </div>
     
