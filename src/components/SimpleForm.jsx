093695f9a8f3a78849b317c85bea66b3411aa99e
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-16 13:49:45 -0400

    Update

--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -4,6 +4,17 @@ const SimpleForm = () => {
   return (
    <>
     <div>SimpleForm</div>
+    <form>
+        <div>
+            Student Name:<input type='text' />
+        </div>
+        <div>
+            Student Father Name:<input type='text' />
+        </div>
+        <div>
+           Student Mother Name:<input type='text' />
+        </div>
+    </form>
     </>
   )
 }
