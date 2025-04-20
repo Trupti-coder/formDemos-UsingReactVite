6cd6e656b59694c0cd44f3e3fba1d4b80c4822f7
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-19 22:09:03 -0400

    Update

--- src/components/PracticeForm.jsx
+++ src/components/PracticeForm.jsx
@@ -4,6 +4,17 @@ const PracticeForm = () => {
   return (
     <>
     <div>PracticeForm</div>
+    <div>
+        Creating a SignUp Form
+    </div>
+    <form>
+      <div>
+        Name:<input type='text' />
+      </div>
+      <div>
+        City:<input type='text' />
+      </div>
+    </form>
     </>
   )
 }
