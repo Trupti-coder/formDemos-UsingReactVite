50c922ade743b1cc01ef36574a0471cd584411eb
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-21 20:12:28 -0400

    Update

--- src/components/NewForm.jsx
+++ src/components/NewForm.jsx
@@ -7,7 +7,10 @@ const NewForm = () => {
      <div>NewForm</div>
      <div>School Championship Registration Form</div>
      <form>
-        
+        <div>
+            School Name:<input type='text' />
+        </div>
+
      </form>
    </>
   )
