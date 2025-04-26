5495404241e14d37dff50e689a181a6ffd31a2f5
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-26 14:10:41 -0400

    update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -77,7 +77,7 @@ const Form = () => {
         </div>
         <div>
             Gender:<input type='radio' name='gender' value="Male"  checked={formData.gender==="Male"} onChange={handleChange}/>Male
-            <input type='radio' name='gender' value="Female" checked={formData.gender==="Female"} />Female
+            <input type='radio' name='gender' value="Female" checked={formData.gender==="Female"} onChange={handleChange} />Female
             <input type='radio'  name='gender' value="Other" checked={formData.gender==="Other"}/>Other
         </div>
         <div>
