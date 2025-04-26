ebdb315d8373445531db62a960350485f200468e
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-25 20:52:22 -0400

    Update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,6 +4,15 @@ const Form = () => {
   return (
     <>
     <div>Form</div>
+    <h1>Creating a normal Form</h1>
+    <form>
+        <div>
+            FirstName:<input type='text' />
+        </div>
+        <div>
+            
+        </div>
+    </form>
     </>
   )
 }
