6b6991917e5381cd851e65e99406d836900b6153
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-26 14:11:57 -0400

    Update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -91,7 +91,7 @@ const Form = () => {
            </select>
         </div>
         <div>
-            Email:<input type='email' placeholder='Enter Email'  name='email' onChange={handleChange}/> 
+            Email:<input type='email' placeholder='Enter Email'  name='email' value={formData.email} onChange={handleChange}/> 
         </div>
         <div>
             Password:<input type='password' placeholder='Enter Password' name='password' onChange={handleChange}/>
