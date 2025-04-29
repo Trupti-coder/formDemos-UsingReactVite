8303962aff3044d3866d1c4bc4f1879c53154d38
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-28 21:20:33 -0400

    update

--- src/components/AuthenticationForm.jsx
+++ src/components/AuthenticationForm.jsx
@@ -23,7 +23,7 @@ const AuthenticationForm = () => {
             Email:<input type='email' placeholder='Enter Email'  name='email'/>
         </div>
         <div>
-            Password:<input type='password' placeholder='Enter Password' />
+            Password:<input type='password' placeholder='Enter Password' name='password' />
         </div>
         <button type='submit'>Submit</button>
      </form>
