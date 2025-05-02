d7dc5e724832da2f3886ca159701bee8bdbd72db
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-05-01 20:21:44 -0400

    Update

--- src/components/AuthenticationForm.jsx
+++ src/components/AuthenticationForm.jsx
@@ -55,7 +55,7 @@ const AuthenticationForm = () => {
             userFirstName:firstName,
             userLastName:lastName,
             userAge:age,
-            
+            userCity:city,
 
         }
 
