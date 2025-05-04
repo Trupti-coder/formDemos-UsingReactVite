cc82bee859070016a629bdbdd60ec3c8b5811ec2
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-05-04 19:35:42 -0400

    Update

--- src/components/AuthenticationForm.jsx
+++ src/components/AuthenticationForm.jsx
@@ -105,7 +105,7 @@ const AuthenticationForm = () => {
             CityName:<input type='text'  placeholder='Enter CityName' name='cityName'onChange={cityNameChange} required className={styles.input} />
         </div>
         <div>
-            Email:<input type='email' placeholder='Enter Email'  name='email' onChange={emailChange} required />
+            Email:<input type='email' placeholder='Enter Email'  name='email' onChange={emailChange} required className={styles.input} />
         </div>
         <div>
             Password:<input type='password' placeholder='Enter Password' name='password' onChange={passwordChange} required />
