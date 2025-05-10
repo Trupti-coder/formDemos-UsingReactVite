7dc5bd44eeb6ecf5b78b9172eae38da567c7a047
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-05-09 22:36:25 -0400

    Update

--- src/components/TechForm.jsx
+++ src/components/TechForm.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TechForm = () => {
+
+    let[formData,setFormData]=useState({
+        
+    })
   return (
    <>
     <div>TechForm</div>
